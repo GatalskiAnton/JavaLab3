953491779ec9b9573d0e2dec24e072b557b4558b
Anton <ag629331@gmail.com>
2022-09-23 18:01:29 +0300

    added fixes to task 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Task1 {
     public static String RemoveLeadingZeros(String str) {
-        return str.replaceAll("0+([1-9]+)\\.", "$1.").replaceAll("\\s+", " ");
+        return Arrays.asList(str.split("\\s+")).stream().map(num -> num.replaceAll("0+(.+)\\.|\\s+", "$1.")).collect(Collectors.joining(" "));
     }
 }
